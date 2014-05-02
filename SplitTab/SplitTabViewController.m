0b7daf9f4f2c5d308a255548b638339733dfe7d5
mirandastrand <mstrand@stanford.edu>
2014-05-01 22:56:19 -0700

    Updated prototype allowing user to enter bill and number of guests. Displays the total bill below (i.e. the amount entered by the user)

--- SplitTab/SplitTabViewController.m
+++ SplitTab/SplitTabViewController.m
@@ -14,8 +14,21 @@
 @end
 
 @implementation SplitTabViewController
+@synthesize billLabel;
 
+//
+// Enters the bill from the text field.
+// Called when user presses 'Enter' button
+//
+-(IBAction)Enter_Bill:(id)sender{
+    billLabel.text = _billField.text;
+    [_billField resignFirstResponder];
+}
 
+//
+// Sets the number of guests (always > 0) and
+// updates the label
+//
 - (void)setNumGuests:(int)numGuests
 {
     //make sure there is always at least 1 guest to avoid dividing by 0
@@ -27,20 +40,34 @@
     self.guestsLabel.text = [NSString stringWithFormat:@"Guests: %d", self.numGuests];
 }
 
+//
+// Called when user presses "-" and increments
+// number of guests
+//
 - (IBAction)touchAddGuest:(id)sender {
     self.numGuests++;
 }
 
+//
+// Called when user presses "-" and decrements
+// number of guests
+//
 - (IBAction)touchSubtractGuest:(id)sender {
     self.numGuests--;
 }
 
+//
+// To do if needed
+//
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+//
+// To do if needed
+//
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
