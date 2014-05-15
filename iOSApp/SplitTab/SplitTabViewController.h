94949f456b5160fe28dead9c2e5d984849c3aa1e
mirandastrand <mstrand@stanford.edu>
2014-05-15 15:40:10 -0700

    Small fixes

--- iOSApp/SplitTab/SplitTabViewController.h
+++ iOSApp/SplitTab/SplitTabViewController.h
@@ -10,10 +10,13 @@
 
 @interface SplitTabViewController : UIViewController <UITextFieldDelegate>
 
+//field where bill is entered
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 
+//label for current bill per guest
 @property (weak, nonatomic) IBOutlet UILabel *billLabel;
 
+//called when enter is pressed for bill field
 -(IBAction)Enter_Bill:(id)sender;
 
 
