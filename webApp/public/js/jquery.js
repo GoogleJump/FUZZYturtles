f9b844c7274f8f02b10905f014cc246ca4b5e0ba
Brooke Wenig <brookewenig@gmail.com>
2014-05-20 13:42:33 -0700

    whoops, forgot to include webApp

---
+++ webApp/public/js/jquery.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  var counter = 2;
+  $("#addButton").click(function() {
+    var newTextBoxDiv = $(document.createElement('div')).attr("id", 'TextBoxDiv' + counter);
+
+	newTextBoxDiv.after().html('<label>Guest #'+ counter + ' : </label>' +
+	      '<input type="text" name="textbox' + counter + 
+	      '" id="textbox' + counter + '" value="" >');
+ 
+	newTextBoxDiv.appendTo("#TextBoxesGroup");
+ 
+ 
+	counter++;
+     });
+ 
+     $("#removeButton").click(function () {
+	if(counter==2){
+          alert("Must have at least 1 guest");
+          return false;
+       }   
+ 
+	counter--;
+ 
+        $("#TextBoxDiv" + counter).remove();
+ 
+     });
+ 
+     $("#getButtonValue").click(function () {
+ 
+	var msg = '';
+	for(i=1; i<counter; i++){
+   	  msg += "\n Guest #" + i + " : " + $('#textbox' + i).val();
+	}
+    	  alert(msg);
+     });
+  });
