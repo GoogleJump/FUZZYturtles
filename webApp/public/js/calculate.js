ce38d754e9c8490a43ab0736e70b01cb2b4c2ba7
Brooke Wenig <brookewenig@gmail.com>
2014-07-04 20:53:01 -0700

    Updated UI, refactored code

--- webApp/public/js/calculate.js
+++ webApp/public/js/calculate.js
@@ -1,28 +1,20 @@
-function calculateTip()
-{
-
-}
-
 function check() {
-if(document.getElementById("total").value != "")
-{
-    document.getElementsByName("tip")[0].disabled = false;
-	document.getElementsByName("tip")[1].disabled = false;
-	document.getElementsByName("tip")[2].disabled = false;
-	document.getElementsByName("tip")[3].disabled = false;
-}
-else
-    document.getElementsByName('tip').disabled = true;
+    if(document.getElementById("total").value !== "" && document.getElementById("numGuests").value !== "") {
+        document.getElementsByName("tip")[0].disabled = false;
+        document.getElementsByName("tip")[1].disabled = false;
+        document.getElementsByName("tip")[2].disabled = false;
+        document.getElementsByName("tip")[3].disabled = false;
+    }
+    else {
+        document.getElementsByName('tip').disabled = true;
+    }
 }
 
-function reply_click(clicked_id)
-{
-	var tip = parseFloat(clicked_id)/100; //this.id will pass in 10 or 20, convert to percent
-	var total = document.getElementById("total").value;
-	var withtip = total * (1+tip);
-	var numGuests = document.getElementById("numGuests").value;
-	var perGuest = Math.round(withtip/numGuests * 100)/100;
-	//alert(perGuest);
-	//var formInput = perGuest;
-	document.getElementById('newText').innerHTML = perGuest;
+function reply_click(clicked_id) {
+    var tip = parseFloat(clicked_id)/100, //this.id will pass in 10 or 20, convert to percent
+        total = document.getElementById("total").value,
+        withtip = total * (1 + tip),
+        numGuests = document.getElementById("numGuests").value,
+        perGuest = Math.round(withtip/numGuests * 100)/100;
+    document.getElementById('newText').innerHTML = '$' + perGuest;
 }
--- webApp/public/js/jquery.js
+++ webApp/public/js/jquery.js
@@ -1,44 +1,32 @@
 $(document).ready(function() {
   $('#otherTip').hide();
-
-  //document.getElementById("otherTip").hide();
   var counter = 2;
 
   $("#addButton").click(function() {
     var newTextBoxDiv = $(document.createElement('div')).attr("id", 'TextBoxDiv' + counter);
-  	newTextBoxDiv.after().html('<label>Guest #'+ counter + ' : &nbsp</label>' +
-  	      '<input type="text" name="textbox' + counter + 
-  	      '" id="textbox' + counter + '" value="" >' + 
-          '&nbsp<label>Amount : &nbsp</label>' +
-          '<input type="text" name="amtbox" id="amtbox' + counter + '" value="" >');
-  	newTextBoxDiv.appendTo("#TextBoxesGroup");
+    newTextBoxDiv.after().html('<label>Guest #'+ counter + ' : &nbsp</label>' +
+                               '<input type="text" name="textbox' + counter +
+                               '" id="textbox' + counter + '" value="" >' + 
+                               '&nbsp<label>Amount : &nbsp</label>' +
+                               '<input type="text" name="amtbox" id="amtbox' + counter + '" value="" >');
+    newTextBoxDiv.appendTo("#TextBoxesGroup");
   
-
     var newNewText = $(document.createElement('p')).attr("id", 'NewTextDiv' + counter);
     newNewText.after().html(
       'Amount for Guest ' + counter + ': <span name="newText" id="newText' + counter + '"></span>'
-      );
+    );
     newNewText.appendTo("#NewTextBoxesGroup");
       counter++;
   });
  
   $("#removeButton").click(function () {
-  	if(counter==2){
+    if (counter==2) {
             alert("Must have at least 1 guest");
             return false;
          }   
-  	counter--;
+    counter--;
     $("#TextBoxDiv" + counter).remove();
-    $("#NewTextDiv" + counter).remove(); //TODO: make sure it removes "amount for guest x"
+    $("#NewTextDiv" + counter).remove();
   });
 
 });
-  /*   $("#getButtonValue").click(function () {
- 
-	var msg = '';
-	for(i=1; i<counter; i++){
-   	  msg += "\n Guest #" + i + " : " + $('#textbox' + i).val();
-	}
-    	  alert(msg);
-     }); */
-  
