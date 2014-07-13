8619da0b015e8ad77eecbb85fd39e53c5c7ced3c
Brooke Wenig <brookewenig@gmail.com>
2014-07-12 22:06:27 -0700

    Finished webapp, minus the FB login - advanced features fully fleshed out

--- webApp/public/js/advCalculate.js
+++ webApp/public/js/advCalculate.js
@@ -1,37 +1,59 @@
-function check() {
-    if (document.getElementById("amtbox1").value !== "") {
-        document.getElementsByName("tip")[0].disabled = false;
-        document.getElementsByName("tip")[1].disabled = false;
-        document.getElementsByName("tip")[2].disabled = false;
-        document.getElementsByName("tip")[3].disabled = false;
-    }
-    else {
-        document.getElementsByName('tip').disabled = true;
-    }
+function showOtherTip() {
+	document.getElementById("otherTip").value = '';
+
+	$('#otherTipDiv').show();
+	var tip = 0;
+	var x = document.getElementsByName("amtbox");
+	var len = x.length;
+	for (var i =1; i < len+1; i++){
+		$('#newText' + i).text(function(){
+			var total = document.getElementById("amtbox"+i).value,
+                tax = document.getElementById("tax").value;
+			total = total*tax/100 + total*(1+tip);
+			var perGuest = Math.round(total*100)/100;
+			return '$' + perGuest;
+		});
+	}
 }
 
-function checkOther() {
-	$('#otherTip').show();
-}
-
-function replyOtherClick(clicked_id) {
+function clickedOther(clicked_id) {
 	var tip = document.getElementById("otherTip").value;
 	tip = tip/100;
 	var x = document.getElementsByName("amtbox"),
         len = x.length;
 	for (var i =1; i < len+1; i++) {
 		$('#newText' + i).text(function(){
-			var total = document.getElementById("amtbox"+i).value;
-			var tax = document.getElementById("tax").value;
+			var total = document.getElementById("amtbox"+i).value,
+				tax = document.getElementById("tax").value;
 			total = total*tax/100 + total*(1+tip);
 			var perGuest = Math.round(total*100)/100;
-			return perGuest;
+			return ('$' + perGuest);
 		});
 	}
 }
 
-function reply_click(clicked_id) {
-	$('#otherTip').hide();
+function updateAmountTax() {
+	//$('#otherTipDiv').hide();
+	//$('#otherTip').innerHTML = "";
+	var radios = $("input[type='radio']");
+	var checkRadios = radios.filter(":checked"); //Have to check for other case!!!!
+	var otherTip = document.getElementById("otherTip").value;
+
+	var tip = 0;
+	if (checkRadios.length > 0){
+		tip = checkRadios[0].id;
+		if (tip === "other") {
+			tip = otherTip;
+		}
+	}
+
+	return updateTip(tip);
+}
+function hideOtherTip() {
+	$('#otherTipDiv').hide();
+}
+
+function updateTip(clicked_id) {
 	var tip = parseFloat(clicked_id)/100; //this.id will pass in 10 or 20, convert to percent
 	var x = document.getElementsByName("amtbox");
 	var len = x.length;
@@ -41,12 +63,13 @@ function reply_click(clicked_id) {
                 tax = document.getElementById("tax").value;
 			total = total*tax/100 + total*(1+tip);
 			var perGuest = Math.round(total*100)/100;
-			return perGuest;
+			return '$' + perGuest;
 		});
 	}
 }
 
-    
-function updateNameBox() {
-    debugger
+function updateNameBox(currId) {
+	var name = document.getElementById(currId).value;
+	var counter = currId.replace('textbox','');
+	$('#guestName'+counter).html('Amount for ' + name + ': ' + '<span name="newText" id="newText' + counter + '"></span>' );
 }
