853128a2033840d583a8cd47a394eb45b9c9dfb2
mirandastrand <mstrand@stanford.edu>
2014-07-20 20:46:40 -0700

    ported main helper classes and methods to iOS app

---
+++ iOSApp/SplitTab/Split.h
@@ -0,0 +1,13 @@
+//
+//  Split.h
+//  SplitTab
+//
+//  Created by Miranda Strand on 7/20/14.
+//  Copyright (c) 2014 FUZZYturtles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Split : NSObject
+
+@end
---
+++ iOSApp/SplitTab/Split.m
@@ -0,0 +1,13 @@
+//
+//  Split.m
+//  SplitTab
+//
+//  Created by Miranda Strand on 7/20/14.
+//  Copyright (c) 2014 FUZZYturtles. All rights reserved.
+//
+
+#import "Split.h"
+
+@implementation Split
+
+@end
