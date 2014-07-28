4b579bbd3beb3e2ad2846d50924890372a21e065
mirandastrand <mstrand@stanford.edu>
2014-07-27 21:00:52 -0700

    integrated algorithm with iOS app

---
+++ iOSApp/SplitTab/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  SplitTab
+//
+//  Created by Miranda Strand on 7/26/14.
+//  Copyright (c) 2014 FUZZYturtles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
